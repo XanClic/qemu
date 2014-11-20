30589db91518a4cbef14409e8f1776dce4a04984
Max Reitz <mreitz@redhat.com>
2014-11-20 11:19:04 +0100

    qmp: Introduce blockdev-change-medium
    Introduce a new QMP command 'blockdev-change-medium' which is intended
    to replace the 'change' command for block devices. The existing function
    qmp_change_blockdev() is accordingly renamed to
    qmp_blockdev_change_medium().
    
    Signed-off-by: Max Reitz <mreitz@redhat.com>

--- blockdev.c
+++ blockdev.c
@@ -2102,8 +2102,9 @@ void qmp_blockdev_insert_medium(const char *device, const char *node_name,
     qmp_blockdev_insert_anon_medium(device, bs, errp);
 }
 
-void qmp_change_blockdev(const char *device, const char *filename,
-                         const char *format, Error **errp)
+void qmp_blockdev_change_medium(const char *device, const char *filename,
+                                bool has_format, const char *format,
+                                Error **errp)
 {
     BlockBackend *blk;
     BlockBackendRootState *blk_rs;
@@ -2127,7 +2128,7 @@ void qmp_change_blockdev(const char *device, const char *filename,
     bdrv_flags = blk_rs->read_only ? 0 : BDRV_O_RDWR;
     bdrv_flags |= blk_rs->open_flags & ~BDRV_O_RDWR;
 
-    if (format) {
+    if (has_format) {
         drv = bdrv_find_whitelisted_format(format, bdrv_flags & BDRV_O_RDWR);
         if (!drv) {
             error_setg(errp, "Invalid block format '%s'", format);
