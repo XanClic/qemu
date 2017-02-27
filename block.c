87919be21b62a52f177c30909f3344fdaa2784c5
Max Reitz <mreitz@redhat.com>
2017-02-27 18:12:59 +0100

    block: Add PreallocMode to bdrv_truncate()
    For block drivers that just pass a truncate request to the underlying
    protocol, we can now pass the preallocation mode instead of aborting if
    it is not PREALLOC_MODE_OFF.
    
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>

--- block.c
+++ block.c
@@ -3268,7 +3268,8 @@ exit:
 /**
  * Truncate file to 'offset' bytes (needed only for file protocols)
  */
-int bdrv_truncate(BdrvChild *child, int64_t offset, Error **errp)
+int bdrv_truncate(BdrvChild *child, int64_t offset, PreallocMode prealloc,
+                  Error **errp)
 {
     BlockDriverState *bs = child->bs;
     BlockDriver *drv = bs->drv;
@@ -3289,7 +3290,7 @@ int bdrv_truncate(BdrvChild *child, int64_t offset, Error **errp)
         return -EACCES;
     }
 
-    ret = drv->bdrv_truncate(bs, offset, PREALLOC_MODE_OFF, errp);
+    ret = drv->bdrv_truncate(bs, offset, prealloc, errp);
     if (ret == 0) {
         ret = refresh_total_sectors(bs, offset >> BDRV_SECTOR_BITS);
         bdrv_dirty_bitmap_truncate(bs);
