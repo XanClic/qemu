362a2df62fdee90e098eb874e23b5f65aeee3245
Max Reitz <mreitz@redhat.com>
2017-03-13 22:15:15 +0100

    block/qcow2: Lock s->lock in preallocate()
    preallocate() is and will be called only from places that do not lock
    s->lock: Currently that is qcow2_create2(), as of a future patch it will
    be called from qcow2_truncate(), too.
    
    It therefore makes sense to move locking that mutex into preallocate()
    itself.
    
    Signed-off-by: Max Reitz <mreitz@redhat.com>

--- block/qcow2.c
+++ block/qcow2.c
@@ -2043,11 +2043,16 @@ static int qcow2_change_backing_file(BlockDriverState *bs,
  */
 static int preallocate(BlockDriverState *bs, int64_t offset, int64_t new_length)
 {
+    BDRVQcow2State *s = bs->opaque;
     uint64_t bytes;
     uint64_t host_offset = 0;
     unsigned int cur_bytes;
     int ret;
     QCowL2Meta *meta;
+
+    if (qemu_in_coroutine()) {
+        qemu_co_mutex_lock(&s->lock);
+    }
 
     assert(offset <= new_length);
     bytes = new_length - offset;
@@ -2057,7 +2062,7 @@ static int preallocate(BlockDriverState *bs, int64_t offset, int64_t new_length)
         ret = qcow2_alloc_cluster_offset(bs, offset, &cur_bytes,
                                          &host_offset, &meta);
         if (ret < 0) {
-            return ret;
+            goto done;
         }
 
         while (meta) {
@@ -2067,7 +2072,7 @@ static int preallocate(BlockDriverState *bs, int64_t offset, int64_t new_length)
             if (ret < 0) {
                 qcow2_free_any_clusters(bs, meta->alloc_offset,
                                         meta->nb_clusters, QCOW2_DISCARD_NEVER);
-                return ret;
+                goto done;
             }
 
             /* There are no dependent requests, but we need to remove our
@@ -2094,11 +2099,17 @@ static int preallocate(BlockDriverState *bs, int64_t offset, int64_t new_length)
         ret = bdrv_pwrite(bs->file, (host_offset + cur_bytes) - 1,
                           &data, 1);
         if (ret < 0) {
-            return ret;
+            goto done;
         }
     }
 
-    return 0;
+    ret = 0;
+
+done:
+    if (qemu_in_coroutine()) {
+        qemu_co_mutex_unlock(&s->lock);
+    }
+    return ret;
 }
 
 static int qcow2_create2(const char *filename, int64_t total_size,
@@ -2318,10 +2329,7 @@ static int qcow2_create2(const char *filename, int64_t total_size,
 
     /* And if we're supposed to preallocate metadata, do that now */
     if (prealloc != PREALLOC_MODE_OFF) {
-        BDRVQcow2State *s = blk_bs(blk)->opaque;
-        qemu_co_mutex_lock(&s->lock);
         ret = preallocate(blk_bs(blk), 0, total_size);
-        qemu_co_mutex_unlock(&s->lock);
         if (ret < 0) {
             error_setg_errno(errp, -ret, "Could not preallocate metadata");
             goto out;
