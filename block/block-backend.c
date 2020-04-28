861f4e291417a3d0a59cc8a4935cacdfa5dca549
Max Reitz <mreitz@redhat.com>
2020-04-28 15:14:48 +0200

    block: Add blk_make_empty()
    Two callers of BlockDriver.bdrv_make_empty() remain that should not call
    this method directly.  Both do not have access to a BdrvChild, but they
    can use a BlockBackend, so we add this function that lets them use it.
    
    Signed-off-by: Max Reitz <mreitz@redhat.com>

--- block/block-backend.c
+++ block/block-backend.c
@@ -2402,3 +2402,8 @@ const BdrvChild *blk_root(BlockBackend *blk)
 {
     return blk->root;
 }
+
+int blk_make_empty(BlockBackend *blk, Error **errp)
+{
+    return bdrv_make_empty(blk->root, errp);
+}
